be21e1798ef94827100065d2e90e23ee3478a963
ruhtar <arthur.amorim10@gmail.com>
2024-02-14 20:12:44 -0300

    voiceTimeInChat - wip

--- src/index.ts
+++ src/index.ts
@@ -3,41 +3,47 @@ export { client };
 
   import { ExtendedClient } from "./structs/ExtendedClient";
 
+const startTimePerUser = new Map<string, number>();
 const voiceTime = new Map<string, number>();
 const client = new ExtendedClient();
 client.start();
 
 client.on('voiceStateUpdate', (oldState, newState) => {
-  const channelId = newState.channel?.id;
-  const userId = newState.member?.id;
+  const member = oldState.member || newState.member;
+  const userId = member?.id;
 
-  if (channelId && userId) { // Entrou em um canal
+  if (newState.channel && userId && !oldState.channel) {
+    // Entrou em um canal
     console.log("Entrou em um canal");
-    const startTime = Math.floor(Date.now() / 1000); // Converte para segundos
 
-    // Iniciar contagem de tempo se ainda não estiver em execução
+    startTimePerUser.set(userId, Math.floor(Date.now() / 1000));
     if (!voiceTime.has(userId)) {
-      console.log("setou como", startTime)
-      voiceTime.set(userId, startTime);
-    }
-
-    if (oldState.channel?.id) { // Saiu de um canal
-      console.log("Saiu do canal");
-      const endTime = Math.floor(Date.now() / 1000); // Converte para segundos
-
-      const startTime = voiceTime.get(userId);
-
-      if (startTime) {
-        const timeInVoiceToAdd = endTime - startTime;
-        console.log('timeInVoiceToAdd',timeInVoiceToAdd)
-
-        if (voiceTime.has(userId)) {
-          voiceTime.set(userId, startTime + timeInVoiceToAdd);
-        }
-      }
+      // Se o usuário não estava em um canal antes, inicializa o tempo
+      voiceTime.set(userId, 0);
     }
   }
-  console.log("voiceTime",voiceTime.get(userId!));
+
+  if (oldState.channel && userId && !newState.channel) {
+    // Saiu de um canal
+    console.log("Saiu do canal");
+    
+    const endTime = Math.floor(Date.now() / 1000); // Converte para segundos
+    const startTime = startTimePerUser.get(userId);
+
+    const timeInVoiceToAdd = endTime - startTime!;
+    console.log('timeInVoiceToAdd', timeInVoiceToAdd);
+
+    const oldVoiceTime = voiceTime.get(userId) || 0;
+    console.log('oldVoiceTime', oldVoiceTime);
+
+    const newVoiceTime = oldVoiceTime + timeInVoiceToAdd;
+    console.log("setou", newVoiceTime);
+
+    voiceTime.set(userId, newVoiceTime);
+    
+    startTimePerUser.set(userId, 0); //reset
+  }
+  console.log("voiceTime", voiceTime.get(userId!));
 });
 
 client.on("ready", () => {
@@ -45,7 +51,7 @@ client.on("ready", () => {
 });
 
 client.on("messageCreate", (message) => {
-  if (message.author.id == client.user?.id) return; // Evita que Riquelme responda a si mesmo.
+  if (message.author.id === client.user?.id) return; // Evita que Riquelme responda a si mesmo.
 
   // message.reply({
   //     content: `Eae man, ${message.author.displayName}, ${message.author.username}, ${message.author.globalName}`
