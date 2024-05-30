bddd3d1d2d8b72a3baa25584e5b5d81552a45b94
ruhtar <arthur.amorim10@gmail.com>
2024-05-30 00:43:50 -0300

    Changes commit message

--- auto_commit_push.ps1
+++ auto_commit_push.ps1
@@ -1,11 +1,7 @@
-# Mensagem de commit
-$commitMessage = "This an automatic commit to save the sqlite archive"
+$commitMessage = "This an automatic commit to save the sqlite archive to the github repo"
 
-# Adicione todos os arquivos ao staging
 git add .
 
-# Crie o commit com a mensagem especificada
 git commit -m $commitMessage
 
-# Faça o push para o repositório remoto
 git push origin master
